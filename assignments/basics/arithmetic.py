5cf3791485d06c03811a86d78d59fe5c9b08eda0
Brettlandis1 <brettxlandis@gmail.com>
2022-09-12 21:42:06 -0400

    Added 3 new files
    artihmetic pri and tests

---
+++ assignments/basics/arithmetic.py
@@ -0,0 +1,34 @@
+def add(a, b):
+    '''
+    Returns the result of a plus b.
+    '''
+    return (1 + 2)
+
+def subtract(a, b):
+    '''
+    Returns the result of a minus b.
+    '''
+    return (2 - 1)
+def multiply(a, b):
+    '''
+    Returns the result of a times b.
+    '''
+    return (2 * 2)
+
+def divide(a, b):
+    '''
+    Returns the result of a divided by b.
+    '''
+    return (4 / 2)
+
+def power(a, b):
+    '''
+    Returns the result of a to the power of b.
+    '''
+    return (2^3)
+
+def mod(a, b):
+    '''
+    Returns the result of a modulo b.
+    '''
+    return (7 % 2)
---
+++ assignments/basics/primitives.py
@@ -0,0 +1,26 @@
+from tkinter.tix import Tree
+
+
+def get_int():
+    '''
+    Returns a variable of type 'int'.
+    '''
+    return (3)
+
+def get_float():
+    '''
+    Returns a variable of type 'float'.
+    '''
+    return (2.6)
+
+def get_str():
+    '''
+    Returns a variable of type 'str'.
+    '''
+    return (Tree)
+
+def get_bool():
+    '''
+    Returns a variable of type 'bool'.
+    '''
+    return (True)
