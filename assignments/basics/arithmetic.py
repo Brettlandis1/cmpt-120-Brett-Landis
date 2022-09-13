afc07e93a86819d0275a38c7a6901134e328c630
Brettlandis1 <brettxlandis@gmail.com>
2022-09-12 21:52:52 -0400

    Fixed paramter syntax errors
    Improved commenting and styling

--- assignments/basics/arithmetic.py
+++ assignments/basics/arithmetic.py
@@ -1,34 +1,23 @@
-def add(a, b):
-    '''
-    Returns the result of a plus b.
-    '''
-    return (1 + 2)
+# add two numbers together 
+def add(number1, number2):
+    return (number1 + number2)
 
+# Returns the result of a minus b.
 def subtract(a, b):
-    '''
-    Returns the result of a minus b.
-    '''
-    return (2 - 1)
+    return (a - b)
+
+# Returns the result of a times b.  
 def multiply(a, b):
-    '''
-    Returns the result of a times b.
-    '''
-    return (2 * 2)
+    return (a * b)
 
+# Returns the result of a divided by b.  
 def divide(a, b):
-    '''
-    Returns the result of a divided by b.
-    '''
-    return (4 / 2)
+    return (a / b)
 
+# Returns the result of a to the power of b.  
 def power(a, b):
-    '''
-    Returns the result of a to the power of b.
-    '''
-    return (2^3)
+    return (a^b)
 
+# Returns the result of a modulo b.  
 def mod(a, b):
-    '''
-    Returns the result of a modulo b.
-    '''
-    return (7 % 2)
+    return (a % b)
--- assignments/basics/primitives.py
+++ assignments/basics/primitives.py
@@ -2,25 +2,16 @@ from tkinter.tix import Tree
 
 # a method to get an int
 def get_int():
-    '''
-    Returns a variable of type 'int'.
-    '''
     return (3)
 
+# Returns a variable of type 'float'.    
 def get_float():
-    '''
-    Returns a variable of type 'float'.
-    '''
     return (2.6)
 
+# Returns a variable of type 'str'.    
 def get_str():
-    '''
-    Returns a variable of type 'str'.
-    '''
     return (Tree)
 
+# return a variable of type boolean
 def get_bool():
-    '''
-    Returns a variable of type 'bool'.
-    '''
     return (True)
